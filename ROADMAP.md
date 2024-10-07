1ee744c33829d7784ae5d9b6ae297d626723ce0c
gerMdz <gmontivero@jus.mendoza.gov.ar>
2024-10-07 08:48:42 -0300

    Updates junto con LaraCast. Agregando atributos

--- ROADMAP.md
+++ ROADMAP.md
@@ -2,7 +2,7 @@ Curso de laravel 10 en youtube
 
 [Completo](https://www.youtube.com/watch?v=3e1IsZJuYAw&list=PLZ2ovOgdI-kWWS9aq8mfUDkJRfYib-SvF)
 
-[Cap11](https://www.youtube.com/watch?v=MUOKdpbgbNk&list=PLZ2ovOgdI-kWWS9aq8mfUDkJRfYib-SvF&index=12)
+[Cap16](https://www.youtube.com/watch?v=bisZbFOB_Io&list=PLZ2ovOgdI-kWWS9aq8mfUDkJRfYib-SvF&index=17)
 
 #### Borrar tablas 
 php artisan migrate:rollback
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,4 +43,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function name(): Attribute
+    {
+        return new Attribute(
+            get: static fn($valor) => ucwords($valor),
+            set: static fn($valor) => strtolower($valor)
+        );
+    }
 }
